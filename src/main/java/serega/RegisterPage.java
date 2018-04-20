3637c54baa14674a53961979e9db5853f2aba5d4
Serg Kosh <lightbon@gmail.com>
2018-04-20 15:43:52 -0700

    This is from Day7 task, Registration page

---
+++ src/main/java/serega/RegisterPage.java
@@ -0,0 +1,33 @@
+package serega;
+//class for the Registration Page
+public class RegisterPage {
+	private String name;
+	private String email;
+	private String password;
+	private String retypePassword;
+
+	public RegisterPage(String name, String email, String password, String retypePassword) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.retypePassword = retypePassword;
+
+	}
+
+	public void inputName() {
+
+	}
+	public void inputEmail() {
+		
+	}
+	public void inputPassword() {
+		
+	}
+	public void inputRetypePassword() {
+		
+	}
+	public void clickContinueRegistrationButton() {
+		
+	}
+
+}
