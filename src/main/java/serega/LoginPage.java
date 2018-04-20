98e4a5156ddae967c345b445aa892ac4c8e27b59
Serg Kosh <lightbon@gmail.com>
2018-04-20 15:36:44 -0700

    This is from Day7 task, Login page

---
+++ src/main/java/serega/LoginPage.java
@@ -0,0 +1,30 @@
+package serega;
+
+public class LoginPage {
+	private String email;
+	private String password;
+	
+	public LoginPage(String emailAddress, String pass) {
+		email = emailAddress;
+		password = pass;
+				
+	}
+	public void clickLoginButton() {
+		
+	}
+	public void clickRegisterNowButton() {
+		
+	}
+	public void clickForgotYourPassword() {
+		
+	}
+	
+	public void fillEmailOut(String emailId) {
+		email = emailId;
+	}
+	public void fillPasswordOut(String passId) {
+		password = passId;
+	}
+	
+
+}
