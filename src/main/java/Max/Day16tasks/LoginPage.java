d2e9e12e3a15fab1ed047ae1bf66b434f3175fa2
max8987 <max8987@gmail.com>
2018-04-23 14:34:11 -0700

    Day 16 Tasks

---
+++ src/main/java/Max/Day16tasks/LoginPage.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+//class for Login page
+public class LoginPage {
+	
+	//methods for actual behavior	
+	public static void register(WebDriver driver, String registerNowXpath, String registerPageUrl) throws InterruptedException {
+		//click on Register Now button
+		Thread.sleep(3000);                                                                               
+		WebElement registerNow = driver.findElement(By.xpath(registerNowXpath));
+        registerNow.click();
+    	System.out.print("TEST CASE Register: ");
+        if (driver.getCurrentUrl().equals(registerPageUrl)) {
+        	System.out.println("PASS");
+        }else {
+        	System.out.println("FAIL");
+        }		
+	}
+	
+	public static void forgotPassString (WebDriver driver, String forgotPassXpath, String forgotPassUrl) throws InterruptedException {
+		//click on forgot your password link
+		Thread.sleep(1000);
+		WebElement forgotPass = driver.findElement(By.xpath(forgotPassXpath));
+        forgotPass.click();
+    	System.out.print("TEST CASE Forgot password: ");
+        if (driver.getCurrentUrl().equals(forgotPassUrl)) {
+        	System.out.println("PASS");
+        }else {
+        	System.out.println("FAIL");
+        }    
+	}
+	
+	public static void enterEmail(WebDriver driver, String email, String emailFieldXpath) {
+		//enter email
+		WebElement emailField = driver.findElement(By.xpath(emailFieldXpath));
+        emailField.sendKeys(email);
+    	System.out.print("TEST CASE Enter email address: ");
+        if (emailField.getAttribute("value").equals(email)) {
+        	System.out.println("PASS");
+        }else {
+        	System.out.println("FAIL");
+        }    
+	}
+	
+	public static void enterPassword(WebDriver driver, String password, String passwordFieldXpath) {
+		//enter password
+		WebElement passwordField = driver.findElement(By.xpath(passwordFieldXpath));
+        passwordField.sendKeys(password);
+	}
+	
+	public static void submitLogin(WebDriver driver, String loginButtonXpath, String homePageUrl) throws InterruptedException {
+		//click submit button
+		Thread.sleep(2000);
+		WebElement loginButton = driver.findElement(By.xpath(loginButtonXpath));
+        loginButton.click();
+		Thread.sleep(1000);
+    	System.out.print("TEST CASE Login: ");
+        if (driver.getCurrentUrl().equals(homePageUrl)) {
+        	System.out.println("PASS");	
+        }else {
+        	System.out.println("FAIL");
+        }    
+	}
+			
+}
