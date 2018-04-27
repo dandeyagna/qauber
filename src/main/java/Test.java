b425c653a849068349dcdea8c40d8799038e0b3d
max8987 <max8987@gmail.com>
2018-04-27 09:54:26 -0700

    Test commit on Max branch

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,8 @@
 public class Test{
 	//This is checked in from branch_april_20
 	//This is just a comment
-	public static void main(String[] args){
+	//This came from Max branch
+		public static void main(String[] args){
 		System.out.println("Hello World!!");
 	}
 }
