9b7cb6f89ccbcb526687af054d7e3b569d585564
Serg Kosh <lightbon@gmail.com>
2018-05-03 19:38:11 -0700

    Message

--- src/main/java/serega/TestNgTest.java
+++ src/main/java/serega/TestNgTest.java
@@ -1,5 +1,15 @@
 package serega;
 
+import org.testng.annotations.Test;
+
 public class TestNgTest {
 
+@Test
+public void test1() {
+	System.out.println("test1");
+}
+@Test
+public void test2() {
+	System.out.println("test2");
+}
 }
